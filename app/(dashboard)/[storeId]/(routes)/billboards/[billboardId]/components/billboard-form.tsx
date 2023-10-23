d1cf11c467da3781659b44642e9a66b045c6131a
Antareep Hasan <antareephasan@gmail.com>
2023-10-24 03:26:00 +0600

    billboards functions added

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -83,8 +83,8 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
             setLoading(true);
             await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
             router.refresh();
-            router.push('/');
-            toast.success("Billboatd deleted");
+            router.push(`/${params.storeId}/billboards`);
+            toast.success("Billboard deleted");
         } catch (error) {
             toast.error("Make sure you removed all categories using this billboard first.");
         } finally {
@@ -161,8 +161,6 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
                     </Button>
                 </form>
             </Form>
-
-            <Separator />
         </>
     )
 }
